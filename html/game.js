4262f34051cf26a84522a5e2c0a9d34ab65ef628
Anthony <cue0889@gmail.com>
2014-04-27 18:52:16 -0400

    Add top and bottom borders, extend side conveyors in an attemptprevent character escape

--- html/game.js
+++ html/game.js
@@ -202,12 +202,14 @@ function generateBatch() {
 }
 
 var floorObstacles = [
-	new Splat.Entity(0, 0, 108, canvas.height), //left conveyor
-	new Splat.Entity(canvas.width - 108, 0, 108, canvas.height), //right conveyor
+	new Splat.Entity(0, -100, 108, canvas.height + 200), //left conveyor
+	new Splat.Entity(canvas.width - 108, -100, 108, canvas.height + 200), //right conveyor
 	new Splat.Entity(243,108,639,60), //top machine
 	new Splat.Entity(243,324,639,60), //middle machine
 	new Splat.Entity(243, 540, 417, 60), //bottom machine
-	new Splat.Entity(774, 540, 108, 60) // shredder
+	new Splat.Entity(774, 540, 108, 60), // shredder
+	new Splat.Entity(-100, 0, (canvas.width + 200), 60), // top border
+	new Splat.Entity(-100, canvas.height, (canvas.width + 200), 60) // bottom border
 ];
 
 
